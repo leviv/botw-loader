84fd52426f78c0c53eed3bd1f48fbee98572b26b
Levi Villarreal <villarreallevi@gmail.com>
2019-01-06 23:38:29 -0600

    Animate text and divine beasts appearing

--- js/main.js
+++ js/main.js
@@ -2,15 +2,72 @@ $(document).ready(function(){
 
   // Animate the lighter bar expanding when page is loaded
   $("#top-load-section").animate({
-    width: "105%"
+    width: "105%" // set the width to 105 so there's no gap with the buffer zone
   }, {
     duration: 500,
     easing: 'linear'
   });
 
+  // get rid of the extra space we created due to the buffer zone
+  $("#top-load-section")
+  .queue(function (next) {
+    $(this).css('width', '100%');
+    next();
+  });
+
   // Animate the 'buffer zome' between the lighter bar and
   $("#top-wipe").animate({
     left: "-150px"
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
+
+  // Animate the 'tip' description fading in
+  $("#loading-description").delay(500).animate({
+    opacity: "1",
+    left: "48px"
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
+
+  // Animate the 'tip' fading in
+  $("#loading-title").delay(600).animate({
+    opacity: "1",
+    left: "48px"
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
+
+  // Animate the logo fading in
+  $("#loading-logo").delay(750).animate({
+    opacity: "1",
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
+
+  // Animate statistics fading in
+  $("#statistics").delay(1500).animate({
+    opacity: "1",
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
+
+  // Animate beasts fading in
+  $("#divine-beasts").delay(1500).animate({
+    opacity: "1",
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
+
+  // Animate beasts fading in
+  $("#next-tip").delay(4000).animate({
+    opacity: "1",
   }, {
     duration: 500,
     easing: 'linear'
