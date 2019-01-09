4bed9abcab1ca4c3b7f0db0923eb49e8bf09350a
Levi Villarreal <villarreallevi@gmail.com>
2019-01-09 05:38:05 -0600

    Animate the animation fading out

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ data.sort( function() { return 0.5 - Math.random() } );
 var tipCounter = 0;
 
 $(document).ready(function(){
-
   // Originally set the tip to a random tip from the data
   changeTip();
 
@@ -17,89 +16,91 @@ $(document).ready(function(){
       aPressed();
     }
   });
+
+  // This waits until the logo has fully loaded
+  $("#split-logo").promise().done(function(){
+    outroAnimations();
+  });
 });
 
-//Function to switch out the text in the tip section
-function changeTip(){
-  $("#loading-title h3").text(data[tipCounter][0]);
-  $("#loading-description p").text(data[tipCounter][1]);
+// function to handle all of the closing animations
+function outroAnimations(){
 
-  // Increase the tipCounter but make sure we don't go over the array length
-  tipCounter = (tipCounter + 1) % (data.length - 1);
-}
+  // Animate the 'next tip' dialouge fading out
+  $("#next-tip").animate({
+    opacity: "0",
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
 
-// Function to handle what happens if the a key is pressed
-function aPressed() {
-  // Check to see if pressing a is valid at this point
-  if( $("#next-tip").css('opacity') == '1') {
+  // Animate the 'tip' description fading out
+  $("#loading-description").delay(500).animate({
+    opacity: "0",
+    left: "56px"
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
 
-    // Animate the 'next tip' dialouge fading out
-    $("#next-tip").animate({
-      opacity: "0",
-    }, {
-      duration: 500,
-      easing: 'linear'
-    });
+  // Animate the 'tip' fading out
+  $("#loading-title").delay(500).animate({
+    opacity: "0",
+    left: "56px"
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
 
-    // Animate the 'tip' description fading out
-    $("#loading-description").animate({
-      opacity: "0",
-      left: "56px"
-    }, {
-      duration: 500,
-      easing: 'linear'
-    });
+  //Change the css so the tip appears to be coming from the left again
+  $("#loading-description").queue(function (next) {
+    $(this).css('left', '40px');
+    next();
+  });
 
-    // Animate the 'tip' fading out
-    $("#loading-title").animate({
-      opacity: "0",
-      left: "56px"
-    }, {
-      duration: 500,
-      easing: 'linear'
-    });
+  $("#loading-title").queue(function (next) {
+    $(this).css('left', '40px');
+    next();
+  });
 
-    //Change the css so the tip appears to be coming from the left again
-    $("#loading-description").queue(function (next) {
-      $(this).css('left', '40px');
-      next();
-    });
+  // Edit the attributes for the 'buffer zone' to get it ready to wipe
+  $("#top-wipe").addClass('reverse');
+  $("#top-wipe").css('left', 'auto');
+  $("#top-wipe").css('right', '-150px');
 
-    $("#loading-title").queue(function (next) {
-      $(this).css('left', '40px');
-      next();
-    });
+  // Not sure why the width has to be set to +185 instead of +150
+  // It has something to do with the -7deg skewX CSS that the element has on it
+  // But I counldn't figure out why, or how to fix it
+  var width = $('#loading-container').width() + 185;
 
-    // Call the changeTip funtion after the animations have finished
-    setTimeout(function() { changeTip(); }, 500);
+  // Animate the top bar disappearing
+  $("#end-wipe").delay(500).animate({
+    width: width + "px"
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
 
-    // Animate the 'tip' description fading in
-    $("#loading-description").animate({
-      opacity: "1",
-      left: "48px"
-    }, {
-      duration: 500,
-      easing: 'linear'
-    });
+  // Animate the 'buffer zone' between the lighter bar and darker background
+  $("#top-wipe").delay(500).animate({
+    right: "100%",
+    margin: '0 -1px 0 0' // There was a small gap when set to 0
+  }, {
+    duration: 500,
+    easing: 'linear'
+  });
 
-    // Animate the 'tip' fading in
-    $("#loading-title").animate({
-      opacity: "1",
-      left: "48px"
-    }, {
-      duration: 500,
-      easing: 'linear'
-    });
+  // Animate the logo fading out
+  $("#loading-logo").delay(250).animate({
+    opacity: "0",
+  }, {
+    duration: 1000,
+    easing: 'linear'
+  });
 
-    // Animate the 'next tip' dialouge fading in
-    $("#next-tip").delay(2000).animate({
-      opacity: "1",
-    }, {
-      duration: 500,
-      easing: 'linear'
-    });
+  // Animate the loading screen disappearing
+  $("#loading-container").delay(2000).fadeOut(1000);
 
-  }
 }
 
 // function to handle all of the opening animations
@@ -182,7 +183,90 @@ function introAnimations() {
     $("#split-logo").delay(5000).animate({
       width: "100%"
     }, {
-      duration: 10000,
+      duration: 1000,
       easing: 'linear'
     });
+}
+
+//Function to switch out the text in the tip section
+function changeTip(){
+  $("#loading-title h3").text(data[tipCounter][0]);
+  $("#loading-description p").text(data[tipCounter][1]);
+
+  // Increase the tipCounter but make sure we don't go over the array length
+  tipCounter = (tipCounter + 1) % (data.length - 1);
+}
+
+// Function to handle what happens if the a key is pressed
+function aPressed() {
+  // Check to see if pressing a is valid at this point
+  if( $("#next-tip").css('opacity') == '1') {
+
+    // Animate the 'next tip' dialouge fading out
+    $("#next-tip").animate({
+      opacity: "0",
+    }, {
+      duration: 500,
+      easing: 'linear'
+    });
+
+    // Animate the 'tip' description fading out
+    $("#loading-description").animate({
+      opacity: "0",
+      left: "56px"
+    }, {
+      duration: 500,
+      easing: 'linear'
+    });
+
+    // Animate the 'tip' fading out
+    $("#loading-title").animate({
+      opacity: "0",
+      left: "56px"
+    }, {
+      duration: 500,
+      easing: 'linear'
+    });
+
+    //Change the css so the tip appears to be coming from the left again
+    $("#loading-description").queue(function (next) {
+      $(this).css('left', '40px');
+      next();
+    });
+
+    $("#loading-title").queue(function (next) {
+      $(this).css('left', '40px');
+      next();
+    });
+
+    // Call the changeTip funtion after the animations have finished
+    setTimeout(function() { changeTip(); }, 500);
+
+    // Animate the 'tip' description fading in
+    $("#loading-description").animate({
+      opacity: "1",
+      left: "48px"
+    }, {
+      duration: 500,
+      easing: 'linear'
+    });
+
+    // Animate the 'tip' fading in
+    $("#loading-title").animate({
+      opacity: "1",
+      left: "48px"
+    }, {
+      duration: 500,
+      easing: 'linear'
+    });
+
+    // Animate the 'next tip' dialouge fading in
+    $("#next-tip").delay(2000).animate({
+      opacity: "1",
+    }, {
+      duration: 500,
+      easing: 'linear'
+    });
+
+  }
 }
